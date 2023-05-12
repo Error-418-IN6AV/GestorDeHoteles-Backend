78133ec4ddc00c11f3a6017e7df298448930157d
jcifuentes-201084 <jcifuentes-201084@kinal.edu.gt>
2023-05-12 02:06:27 +0200

    Se creo actualizo controller en crud

--- src/reservation/reservation.controller.js
+++ src/reservation/reservation.controller.js
@@ -1,5 +1,4 @@
 'use strict'
-const Hotel = require('../hotel/hotel.model')
 const Room = require('../room/room.model')
 const Reservation = require('./reservation.model')
 
@@ -10,19 +9,11 @@ exports.add = async(req, res)=>{
         //Capturar la data
         let data = req.body;
         //Capturar el id de la person logeada
-        //data.user = req.user.sub
-        //Verificar que existe el hotel
-        let hotelExist = await Hotel.findOne({_id: data.hotel})
-        if(!hotelExist) return res.send({message: 'El hotel no existe'})
-        // Validar que la habitación exista en el hotel indicado
-        let roomExist = await Room.findOne({
-            id: data.room,
-            hotel: data.hotel
-        });
-        if (!roomExist) {
-            return res.status(400).send({ message: 'La habitación no existe en el hotel que puso' });
-        }
-        if(roomExist.status === 'NOAVAILABLE') return res.send({message: 'La habitacion no esta disponible'})
+        data.user = req.user.sub
+        //Verificar que existe la habitacion
+        let roomExist = await Room.findOne({_id: data.room})
+        if(!roomExist) return res.send({message: 'La habitacion no existe'})
+        if(roomExist.available === 'NO DISPONIBLE') return res.send({message: 'La habitacion no esta disponible'})
         // Verificar que el usuario no tenga una reservacion en la misma fecha y habitación
         let reservationExist = await Reservation.findOne({
             $and: [
@@ -31,11 +22,9 @@ exports.add = async(req, res)=>{
                 { user: req.user.sub }
             ]
         });
-        if (reservationExist) {
-            return res.status(400).send({ message: 'You already have a reservation on this date.' });
-        }
+        if (reservationExist) return res.status(400).send({ message: 'You already have a reservation on this date.' });
         //cambiar el estado de la habitacion
-        roomExist.status = 'NOAVAILABLE'
+        roomExist.available = 'NOAVAILABLE'
         await roomExist.save();
         //Crear la reservacion
         let reservation = new Reservation(data);
@@ -57,20 +46,6 @@ exports.getReservations = async(req, res)=>{
     }
 }
 
-exports.getReservationsByHotel = async (req, res) => {
-  const { hotelName } = req.body;
-  try {
-    const reservations = await Reservation.find({ 'hotel.name': hotelName }).populate('hotel room');
-    if (reservations.length === 0) {
-      return res.status(404).send({ message: `No reservations found for ${hotelName}.` });
-    }
-    return res.send({ reservations });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({ message: 'Error getting reservations.' });
-  }
-}
-
 exports.update = async(req, res)=>{
     try{
         //capturar data
@@ -79,14 +54,14 @@ exports.update = async(req, res)=>{
         let reservationId = req.params.id;
         //validar que no se pueda actualizar a una habitacion ocupada
         let roomExist = await Room.findOne({_id: data.room});
-        if (roomExist.status === 'NOAVAILABLE') {
+        if (roomExist.available === 'NO DISPONIBLE') {
             return res.send({message: 'Habitacion ocupada'})
         }
         //obtener reserva actual
         let reservaActual = await Reservation.findOne({_id: reservationId})
         //actualizar bodega anterior
         let roomAnteior = await Room.findOne({_id: reservaActual.room})
-        roomAnteior.status = 'AVAILABLE'
+        roomAnteior.available = 'DISPONIBLE'
         await roomAnteior.save()
         //Actualizar
         let updatedReservation = await Reservation.findOneAndUpdate(
@@ -115,7 +90,7 @@ exports.delete = async(req, res)=>{
         let room = await Room.findById(deletedReservation.room);
         if(!room) return res.status(404).send({message: 'Error finding habitacion'});
         // Cambiar el nombre de la habitacion a "DISPONIBLE"
-        room.status = 'AVAILABLE';
+        room.available = 'DISPONIBLE';
         await room.save();
         return res.send({message: 'reserva deleted sucessfully', deletedReservation});
     }catch(err){
