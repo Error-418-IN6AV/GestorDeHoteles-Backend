6e3ae605cfd4d66b76beed8b5ed388bc2f993e64
Angel <avillanueva-2021436@kinal.edu.gt>
2023-05-16 21:02:07 -0600

    Se agrego ruta del event

--- configs/app.js
+++ configs/app.js
@@ -6,11 +6,16 @@ const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const eventRoutes = require('../src/event/event.routes');
+const typeRoutes = require('../src/type/type.routes');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
+app.use('/event', eventRoutes);
+app.use('/type', typeRoutes);
 
 exports.initServer = ()=>{
     app.listen(port);
