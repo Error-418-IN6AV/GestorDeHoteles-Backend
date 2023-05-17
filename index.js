e4d8d2fda98053e573d239d3549f5f893c659e75
Angel <avillanueva-2021436@kinal.edu.gt>
2023-05-16 21:02:18 -0600

    se agrego el default

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 require('dotenv').config()
 const mongoConfig = require('./configs/mongo');
 const app = require('./configs/app');
+const typeController = require('./src/type/type.controller');
+
 mongoConfig.connect();
 app.initServer();
+typeController.defaultType();
